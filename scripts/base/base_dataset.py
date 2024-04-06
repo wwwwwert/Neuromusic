Fixed gradient accumulation

scripts/base/base_dataset.py
@@ -68,6 +68,7 @@ class BaseDataset(Dataset):
 
     @staticmethod
     def _sort_index(index):
+        return index
         features = list(index[0].keys())
         if "chords_density" in features:
             # number of chords per ms
@@ -83,6 +84,7 @@ class BaseDataset(Dataset):
         midi = Score(path, ttype=TimeUnit.second)
         if self.audio_length != -1 and midi.end() > self.audio_length:
             random_position = random.uniform(0, midi.end() - self.audio_length)
+            random_position = 0
             midi = midi.clip(random_position, random_position + self.audio_length)
             midi = midi.shift_time(-random_position)
         midi = midi.to(TimeUnit.tick)
@@ -107,31 +109,10 @@ class BaseDataset(Dataset):
             tokens = torch.nn.functional.pad(tokens, (0, n_pad), 'constant', value=self.midi_encoder["PAD_None"])
         elif tokens.shape[0] > length:
             random_position = random.randint(0, tokens.shape[0] - length)
+            random_position = 0
             tokens = tokens[random_position:random_position + length]
         return tokens, mask
-    
-    def get_tokens_with_prev(self, midi):
-        tokens_list = self.midi_encoder(midi).ids
-        tokens = torch.zeros(len(tokens_list) + 2, dtype=torch.int32)
-        tokens[0] = self.midi_encoder['BOS_None']
-        tokens[-1] = self.midi_encoder['EOS_None']
-        tokens[1:-1] = torch.tensor(tokens_list)
-        if tokens.shape[0] < self.n_tokens * 2:
-            pred_tokens = tokens[:tokens.shape[0] // 2]
-            
-            next_tokens = tokens[tokens.shape[0] // 2:]
-            next_mask = torch.zeros(self.n_tokens, dtype=torch.int32)
-            next_mask[:next_tokens.shape[0]] = 1
-    
-            n_pad = self.n_tokens - next_tokens.shape[0]
-            next_tokens = torch.nn.functional.pad(next_tokens, (0, n_pad), 'constant', value=self.midi_encoder["PAD_None"])
-        else:
-            random_position = random.randint(0, tokens.shape[0] - self.n_tokens * 2)
-            pred_tokens = tokens[random_position:random_position + self.n_tokens]
-            next_tokens = tokens[random_position + self.n_tokens:random_position + self.n_tokens * 2]
-            next_mask = torch.ones(self.n_tokens, dtype=torch.int32)
-        return next_tokens, next_mask, pred_tokens
-    
+
     @staticmethod
     def _filter_records_from_dataset(
             index: list, max_audio_length, limit