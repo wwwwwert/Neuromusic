d72ebfac5b99ecfacc16a5936c635d710b7b78d3
black_chick <dauspenskiy@edu.hse.ru>
2024-04-10 20:14:47 +0300

    Updated install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,12 @@
 sudo apt update
 sudo apt install fluidsynth
-gem install yadisk
+sudo gem install yadisk
 
 pip install -r requirements.txt
 
 # Load pretrained models
 yadisk https://disk.yandex.ru/d/BV-vu0swn8JiMw
+unzip best_model.zip
+
 yadisk https://disk.yandex.ru/d/721RFMg9um9xOg
+unzip saved.zip
