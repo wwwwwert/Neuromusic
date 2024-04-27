c4f8b9f6ce57dbad422ca51328c4b342f27ab8c9
black_chick <dauspenskiy@edu.hse.ru>
2024-04-27 17:33:21 +0300

    Added Llama model

--- scripts/model/__init__.py
+++ scripts/model/__init__.py
@@ -1,7 +1,9 @@
 from .GPT2.gpt2 import GPT2
+from .Llama.llama import Llama
 from .Music_Transformer.music_transformer import MusicTransformer
 
 __all__ = [
     "MusicTransformer",
-    "GPT2"
+    "GPT2",
+    "Llama"
 ]
